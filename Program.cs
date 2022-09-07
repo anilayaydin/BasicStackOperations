dbd98bcf9c2f1d923ee5e7b20baaa71d8ebe9743
ANIL AYAYDIN <anil.ayaydin@gmail.com>
2022-09-07 13:44:46 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms {
+    class Program {
+
+        static void Main(string[] args) {
+
+            Stack<string> stack = new Stack<string>();
+
+            System.Console.WriteLine("Start Main");
+            stack.Push("Main");
+            System.Console.WriteLine("Start ResponseBuilder");
+            stack.Push("ResponseBuilder");
+            Console.WriteLine("END " + stack.Pop());
+            System.Console.WriteLine("Start CallExternalService");
+            stack.Push("CallExternalService");
+            Console.WriteLine("END " + stack.Pop());
+            System.Console.WriteLine("Start ParseExternalData");
+            stack.Push("ParseExternalData");
+            Console.WriteLine("END " + stack.Pop());
+
+        }
+    }
+}
