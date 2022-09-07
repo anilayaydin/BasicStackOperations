49e85080ba987ea72d76a20030975fa94aa9bc4f
ANIL AYAYDIN <anil.ayaydin@gmail.com>
2022-09-07 13:49:51 +0300

    Changes

--- Program.cs
+++ Program.cs
@@ -12,13 +12,21 @@ namespace Algorithms {
             stack.Push("Main");
             System.Console.WriteLine("Start ResponseBuilder");
             stack.Push("ResponseBuilder");
-            Console.WriteLine("END " + stack.Pop());
             System.Console.WriteLine("Start CallExternalService");
             stack.Push("CallExternalService");
             Console.WriteLine("END " + stack.Pop());
             System.Console.WriteLine("Start ParseExternalData");
             stack.Push("ParseExternalData");
             Console.WriteLine("END " + stack.Pop());
+            Console.WriteLine("END " + stack.Pop());
+            Console.WriteLine("END " + stack.Pop());
+
+
+            //stack.Peek()
+            //stack.TryPeek()
+
+            string item;
+            System.Console.WriteLine(stack.TryPeek(out item));
 
         }
     }
